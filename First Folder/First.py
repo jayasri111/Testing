7bce9009fd60004690651767ef63e00f1f7c1ec6
jayasri111 <jayasri.namburi@symphonize.com>
2024-08-01 09:54:02 +0530

    First commits

---
+++ First Folder/First.py
@@ -0,0 +1,8 @@
+print("Jayasri");
+print("Hello Jaya");
+
+# import keyword
+age = 20;
+print(age);
+import keyword
+print(keyword.kwlist);
---
+++ First Folder/second.py
@@ -0,0 +1,4 @@
+import keyword
+print(keyword.kwlist);
+
+print("jaya", "kittu", "koti", end="\n");
