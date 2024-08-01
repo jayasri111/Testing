ca1eab68ef161603967535d1ce50385a63bf63e8
jayasri111 <jayasri.namburi@symphonize.com>
2024-08-01 18:59:56 +0530

    Second commit

---
+++ New python learning/Lab001.py
@@ -0,0 +1,11 @@
+# name = "jayasri";
+#
+# value = ("This is " +name);
+#
+# Gender = "I am a female";
+# print(value +"\n"+ Gender);
+
+Age = input("In which field ");
+print("I am jaya working in " + Age);
+
+print("jayasri")
